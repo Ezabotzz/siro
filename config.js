538a3e38bc29594ea6f940d086529780c965aa1c
RGA42 <92771260+RGA42@users.noreply.github.com>
2022-06-27 18:56:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,26 +41,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['12012854669'] //ur owner number
+global.ownername = "GadingAgani"
+global.ytname = "YT: Gading Agani" //ur yt chanel name
+global.socialm = "Instagram : @gadingagani" //ur github or insta name
+global.location = "Indonesia" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "SychoBot" //ur bot name
+global.websitex = "https://youtube.com/channel/UCfPxifuwrqmfUBy2JaldYAQ" //ur website
+global.sycho = "https://bit.ly/Sycho-RGA"
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "SychoBot\n\n\n\n\n\n\n🗿🗿🗿🙏 "
+global.author = "GadingAgani"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.ownertag = ['12012854669'] //ur owner tag
+global.ownernummenu = ['12012854669'] //ur owner number in menu and all
+global.watermark = "SychoBot" //ur watermark
+global.botscript = "https://github.com/RGA42/siro"
+global.linkz1 = "https://bit.ly/Sycho-RGA"
+global.linkz2 = "https://bit.ly/Sycho-RGA"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
